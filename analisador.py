2a3a802f73cc02e812922c2f3642214c258fdca0
brunamarques <bruna99stefany@gmail.com>
2020-12-08 19:18:42 -0300

    pronto

--- analisador.py
+++ analisador.py
@@ -18,41 +18,22 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 import imblearn
 from imblearn.under_sampling import RandomUnderSampler
-from data2 import extract_data
+from data import extract_data
 
 
-ngrama = 1
+ngrama = 2
 k = 100
 
-X_sentences, X_prev, X_next, X_pos, Y_sentences = extract_data()
+X_sentences, Y_sentences = extract_data()
 print("tfidf")
-vectorizer = TfidfVectorizer(ngram_range=(1, ngrama))
+vectorizer = TfidfVectorizer(ngram_range=(1, ngrama), stop_words='english')
 X_sentences = vectorizer.fit_transform(X_sentences)
-X_prev = vectorizer.transform(X_prev)
-X_next = vectorizer.transform(X_next)
-
-print(len(vectorizer.get_feature_names()))
-
-print("chi-quadrado")
-selector = SelectKBest(chi2, k=k)
-X_sentences = selector.fit_transform(X_sentences, Y_sentences)
-X_prev = selector.transform(X_prev)
-X_next = selector.transform(X_next)
-
-print("adicionando anterior e posterior")
-X_sentences = hstack([X_sentences, X_prev, X_next, np.expand_dims(np.array(X_pos), -1)])
 
 
 print("\nClassificador SVM: ")
-# undersample = RandomUnderSampler(sampling_strategy='all')
-# #undersample = RandomUnderSampler(sampling_strategy='majority')
-# param_grid = [{'class_weight': ['balanced'],'kernel': ['rbf'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1000]}]
-# clf = GridSearchCV(SVC(), param_grid, cv = 10) 
-# clf = SVC(random_state=0, tol=1e-5)
-clf = NuSVC()
-# clf = LinearSVC(random_state=0, tol=1e-5)
-print("AAA")
-# X_sentences, Y_sentences = undersample.fit_resample(X_sentences, Y_sentences)
+param_grid = [{'C': [1, 10, 100, 1000], 'multi_class':['ovr', 'crammer_singer'], 'penalty':['l1', 'l2'], 'fit_intercept':['True', 'False'], 'max_iter':[1000, 5000]}]
+clf = GridSearchCV(LinearSVC(), param_grid, cv = 10, n_jobs=-1) 
+clf = LinearSVC(random_state=0, tol=1e-5)
 clf = clf.fit(X_sentences, Y_sentences)
 print("Predicão...")
 pred = cross_val_predict(clf, X_sentences, Y_sentences)
@@ -62,61 +43,34 @@ print(classification_report(Y_sentences, pred,  zero_division = 0))
 print("")
 print(confusion_matrix(Y_sentences, pred))
 
-# print('melhores parametros: ', clf.best_params_)
 #################### Random Forest ##########################
-# print("\nClassificador Random Forest:...")
-# param_grid =  [{'class_weight': ['balanced_subsample'], 'max_depth': [20], 'max_features': ['log2'], 'n_estimators': [100]}]
-# #undersample = RandomUnderSampler(sampling_strategy='all')
-# undersample = RandomUnderSampler(sampling_strategy='majority')
-# X_sentences, Y_sentences = undersample.fit_resample(X_sentences, Y_sentences)
-# clf = GridSearchCV(RandomForestClassifier(), param_grid, cv = 10) 
-# clf = clf.fit(X_sentences, Y_sentences)
+print("\nClassificador Random Forest:...")
+param_grid =  [{'n_estimators' :[100, 300, 500],'max_depth' :[5, 8, 15],'min_samples_split' :[2, 5, 10],'min_samples_leaf':[1, 2, 5]}]
+clf = GridSearchCV(RandomForestClassifier(), param_grid, cv = 10, n_jobs=-1) 
+clf = clf.fit(X_sentences, Y_sentences)
 
-# print("Predicão...")
-# pred = cross_val_predict(clf, X_sentences, Y_sentences, cv=10)
+print("Predicão...")
+pred = cross_val_predict(clf, X_sentences, Y_sentences, cv=10)
 
-# print("Classification_report:")
-# print(classification_report(Y_sentences, pred,  zero_division = 0))
-# print("")
-# print(confusion_matrix(Y_sentences, pred))
+print("Classification_report:")
+print(classification_report(Y_sentences, pred,  zero_division = 0))
+print("")
+print(confusion_matrix(Y_sentences, pred))
 
-##################### Naive #############################
-# print("\nClassificador Naive Bayes:")
-# param_grid =  [{'float': [1.0,2.0, 0.5]}]
-# #undersample = RandomUnderSampler(sampling_strategy='all')
-# #undersample = RandomUnderSampler(sampling_strategy='majority')
-# #X_sentences, Y_sentences = undersample.fit_resample(X_sentences, Y_sentences)
-# # clf = GridSearchCV(MultinomialNB(), param_grid, cv = 10) 
-# clf = ComplementNB()
+################### Naive #############################
+print("\nClassificador Naive Bayes:")
+clf = MultinomialNB()
 
-# clf = clf.fit(X_sentences.toarray(), Y_sentences)
+clf = clf.fit(X_sentences, Y_sentences)
 
-# print("Predicão...")
-# pred = cross_val_predict(clf, X_sentences.toarray(), Y_sentences, cv=10)
+print("Predicão...")
+pred = cross_val_predict(clf, X_sentences, Y_sentences, cv=10)
 
-# print("Classification_report:")
-# print(classification_report(Y_sentences, pred,  zero_division = 0))
-# print("")
-# print(confusion_matrix(Y_sentences, pred))
+print("Classification_report:")
+print(classification_report(Y_sentences, pred,  zero_division = 0))
+print("")
+print(confusion_matrix(Y_sentences, pred))
 
-
-# #################### MLP ###############################
-# print("\nClassificador MLP:")
-# param_grid = [{'learning_rate' :['constant', 'invscaling', 'adaptive'], 'activation': ['identity', 'logistic', 'tanh', 'relu'], 'alpha': [0.0001, 0.05]}]
-# undersample = RandomUnderSampler(sampling_strategy='all')
-# #undersample = RandomUnderSampler(sampling_strategy='majority')
-# X_sentences, Y_sentences = undersample.fit_resample(X_sentences, Y_sentences)
-# clf = GridSearchCV(MLPClassifier(), param_grid, cv = 10)
-# clf = clf.fit(X_sentences, Y_sentences)
-
-# print("Predicão...")
-# pred = cross_val_predict(clf, X_sentences, Y_sentences, cv=10)
-
-# print("Classification_report:")
-# print(classification_report(Y_sentences, pred,  zero_division = 0))
-# print("")
-# print(confusion_matrix(Y_sentences, pred))
-# # print(corpus)
 
 
 
--- data.py
+++ data.py
@@ -13,43 +13,13 @@ def cleanhtml(raw_html):
 
 def extract_data():
     aux = []
-    count = 0
     sentences = []
     labels = []
-    sentences_pos = []
-    sentences_previous = []
-    sentences_next = []
     data = pd.read_csv('IMDBDataset.csv', usecols=['review', 'sentiment'])
     print(len(data))
     for i in range(len(data)):    
-        aux2 = cleanhtml(data.values[i][0])
-        aux.append(sent_tokenize(aux2.lower()))
-        # aux.append( word_tokenize(aux2.lower()))
-        # aux.append((aux2.lower()))
-        # aux = cleanhtml(aux)
-        #count+=len(aux[i])
-
-        if(len(aux[i]) >= 2): #tem que ver uma forma de o i ser maior pq não sei quantas sentenças terei
-            for j in range(len(aux[i])):
-                sentences.append(aux[i][j])
-                labels.append(data.values[i][1])
-        else:
-            sentences.append(aux[i][0])
-            labels.append(data.values[i][1])
-
-    for i in range(len(sentences)):
-        sentences_pos.append(i)
-        if i - 1 >= 0:
-            sentences_previous.append(sentences[i - 1])
-        else:
-            sentences_previous.append("")
-
-        if i + 1 < len(sentences):
-            sentences_next.append(sentences[i + 1])
-        else:
-            sentences_next.append("")
-    # print(sentences_previous)
-    # print(len(sentences))
-    return sentences, sentences_previous, sentences_next, sentences_pos, labels
+        sentences.append(cleanhtml(data.values[i][0]))
+        labels.append(data.values[i][1])
+    return sentences, labels
 
 extract_data()
